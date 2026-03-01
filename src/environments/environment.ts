00aefa81bdb79de956c2ebbe9e3db0c0cc3ad18f
Deepak Yadav <112754831+Deepak-25yadav@users.noreply.github.com>
2026-03-01 19:23:56 +0530

    Switch API URL to production endpoint
    changes apiUrl in environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000'
-  // apiUrl: 'https://chatme-be.onrender.com'
+  // apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://chatme-be.onrender.com'
 };
 
